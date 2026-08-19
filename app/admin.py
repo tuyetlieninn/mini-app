296d4cdb56ebb0c391799c01eb1e9983d4361ea3
tuyetlien0406-spec <tuyetlien0406@gmail.com>
2026-08-20 01:50:02 +0900

    clean: keep only week3 folder

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from sqladmin import Admin
-
-
-def setup_admin(app, engine) -> Admin:
-    admin = Admin(app, engine, title="Books Admin")
-    return admin
--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.author import Author
-from app.models.book import Book
-
-__all__ = ["Author"]
